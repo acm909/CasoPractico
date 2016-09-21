f3e71d8ef50e0b94618c0a0b00a581c720acbc97
Antonio Caballero <antoniocaballero.909@gmail.com>
2016-09-21 13:55:25 +0200

    contenido de la clase main

--- Main.java
+++ Main.java
@@ -1,4 +1,36 @@
+import java.util.Scanner;
+
 public class Main{
 	public static void main (String [] args){
+		Scanner sc =new Scanner(System.in;
+		int opc=0;
+		do{
+			verMenu();
+			System.out.println("introduzca una opcion");
+			opc=Integer.parseInt(sc.nextLine());
+			switch (opc){
+			case 1:
+				break;
+			case 2:
+				break;
+				
+			case 3:
+				break;
+			case 4:
+				default:
+					System.out.println("ha elegido salir");
+			}
+			
+			
+		}while(opc!=0);
+		
+		
+		
+	}
+	public static void verMenu(){
+		System.out.println("1.-Sumar");
+		System.out.println("2.-Multiplicar");
+		System.out.println("3.-Dividir");
+		System.out.println("4.-Salir");
 	}
 }
